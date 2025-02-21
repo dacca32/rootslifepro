e97b8b2b1c8b5e94b65bf17e96b51b5b3cb6f370
Scot Darcy <scotdarcymac@gmail.com>
2025-02-21 11:31:44 +0000

    hooked up vite frontend application to docker container scripts. Able to build and run frontend, server, db from container

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    cors: true,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8080/",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
