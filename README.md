052a45d1494045d55105ca9f5791ab4e9c438771
Scot Darcy <scotdarcymac@gmail.com>
2025-02-25 13:57:49 +0000

    added new roles table, created relationship between users and roles table and ran migrations

--- README.md
+++ README.md
@@ -12,6 +12,26 @@ Commands and helpers - @TODO: Tidy and format
 PSQL in the container
 - psql -U <username> -d <database>
 
+    - Adding a new table eg.
+    CREATE TABLE roles ( 
+            id SERIAL PRIMARY KEY,
+            role_name VARCHAR(50) NOT NULL
+            );
+
+    - Insert data eg.
+    INSERT INTO roles (role_name) VALUES ('Admin'), ('User), ('Guest');
+
+    - Add relationship column eg to users table
+    ALTER TABLE "user" ADD COLUMN role_id INTEGER;
+    ALTER TABLE "user"
+    ADD CONSTRAINT fk_role
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id);
+
+    - Next update models and schema to include the new relationship
+
+
+
 Migrations
 - docker exec into webserver
 - flask db migrate -m "message"
--- server/app/models.py
+++ server/app/models.py
@@ -3,12 +3,20 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    role_name = db.Column(db.String(50), unique=True, nullable=False)
+    users = db.relationship('User', backref='role', lazy=True)
+
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(216), nullable=False)
     age = db.Column(db.Integer)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
 
     def set_password(self,password):
         self.password_hash = generate_password_hash(password)
