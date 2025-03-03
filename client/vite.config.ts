dd2c0373efd5cb4235e6e46ca77bef0127a5c04a
Scot Darcy <scotdarcymac@gmail.com>
2025-03-03 12:00:23 +0000

    updated prod docker script and added new caddy prod file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   server: {
-    host: true
+    port: 3000,
+    cors: true,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5000/",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   plugins: [TanStackRouterVite(), react(), tailwindcss()],
 });
